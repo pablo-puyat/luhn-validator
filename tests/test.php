74c58d37f80a40fde97633939a563d26dc4a446e
Thomas Price <tom@cbrz.com>
2021-05-20 16:21:10 -0400

    Updated test script

--- tests/test.php
+++ tests/test.php
@@ -17,3 +17,24 @@ echo "Test 1 result: {$result}\n";
 $result = doTest(79927398710, false) ? 'pass' : 'fail';
 echo "Test 2 result: {$result}\n";
 
+// Test handling invalid input - a string with numbers should be valid as most input from forms will be strings
+$result = doTest('79927398713', true) ? 'pass' : 'fail';
+echo "Test 3 result: {$result}\n";
+
+// Test handling invalid input - any non numeric characters result in an exception being thrown
+try {
+    LuhnValidator::isValid('abc123def');
+    echo "Test 4 result: fail\n";
+} catch (Exception $e) {
+    if ($e->getMessage() === "Invalid input provided")
+    echo "Test 4 result: pass\n";
+}
+
+// Test handling invalid input - numeric strings that are floats or are not integers should throw execption
+try {
+    LuhnValidator::isValid('79927398713.53');
+    echo "Test 5 result: failed\n";
+} catch (Exception $e) {
+    if ($e->getMessage() === "Invalid input provided")
+        echo "Test 5 result: pass\n";
+}
