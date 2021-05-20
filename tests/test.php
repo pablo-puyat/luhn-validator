28873d0bf86534c5ac4d8eb937b29fa088a0ac58
Thomas Price <tom@cbrz.com>
2021-05-20 15:41:29 -0400

    Corrected logic in test script

--- tests/test.php
+++ tests/test.php
@@ -10,10 +10,10 @@ function doTest($number, $success): bool
 }
 
 // Test 1 should return true
-$result = doTest(79927398710, true) ? 'pass' : 'fail';
+$result = doTest(79927398713, true) ? 'pass' : 'fail';
 echo "Test 1 result: {$result}\n";
 
 // Test 2 should return false
-$result = doTest(79927398713, false) ? 'pass' : 'fail';
+$result = doTest(79927398710, false) ? 'pass' : 'fail';
 echo "Test 2 result: {$result}\n";
 
